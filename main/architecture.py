51db45b7d38097e9305f7205187092bfd1ed757f
ajitashwath <ajitashwathr23@lpu.in>
2025-07-04 19:36:43 +0530

    Update architecture.py

--- main/architecture.py
+++ main/architecture.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 def visualize_arch(model):
     try:
-        fig = tf.keras.utils.plot_model(model, show_shapes=True, show_layer_names=True, to_file='model.png')
+        plot_model(model, show_shapes = True, show_layer_names = True, to_file = 'model.png')
         st.image('model.png', caption='Neural Network Architecture')
     except Exception as e:
         st.error(f"Error visualizing architecture: {str(e)}")
